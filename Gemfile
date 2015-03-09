29f7a95c0e02a28ab6bbf3d077096d17bf2f3e15
Casey Clegg <casey.clegg@gmail.com>
2015-03-09 00:07:13 -0700

    initial commit

---
+++ Gemfile
@@ -0,0 +1,20 @@
+ruby '2.0.0'
+source 'https://rubygems.org'
+
+gem "sinatra"
+gem "activerecord"
+gem "sinatra-activerecord"
+gem "twilio-ruby"
+gem 'bcrypt-ruby', '~> 3.0.0'
+gem 'stripe'
+gem 'sendgrid-ruby'
+
+group :development do
+  gem "sqlite3"
+  gem "shotgun"
+  gem "tux"
+end
+
+group :production do
+	gem "pg"
+end
---
+++ app.rb
@@ -0,0 +1,72 @@
+require 'sinatra'
+require 'sinatra/activerecord'
+require 'twilio-ruby'
+require 'stripe'
+require 'sendgrid-ruby'
+
+configure :development do
+ set :database, 'sqlite3:fsr.db'
+ set :show_exceptions, true
+end
+
+configure :production do
+	db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/mydb')
+
+	ActiveRecord::Base.establish_connection(
+		:adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
+		:host     => db.host,
+		:username => db.user,
+		:password => db.password,
+		:database => db.path[1..-1],
+		:encoding => 'utf8'
+	)
+end
+
+class Submission < ActiveRecord::Base
+end
+
+#static pages and sign in
+post '/' do 
+	@submission = Submission.new
+	@submission.all_params = params.to_s
+	@submission.company = params[:company] || ''
+  @submission.country = params[:country] || ''
+  @submission.description1 = params[:description1] || ''
+  @submission.emailAddress = params[:emailAddress].downcase || ''
+  @submission.firstName = params[:firstName] || ''
+  @submission.jobRole = params[:jobRole] || ''
+  @submission.lastName = params[:lastName] || ''
+  @submission.usStates = params[:usStates] || ''
+
+  first_letter = @submission.emailAddress[0]
+  if ('a'..'h').to_a.include?(first_letter)
+  	@submission.bdr = "Casey"
+  	notification_email = "casey@twilio.com"
+  elsif ('i'..'p').to_a.include?(first_letter)
+  	@submission.bdr = "Tom"
+  	notification_email = "tom@twilio.com"
+  elsif ('q'..'z').to_a.include?(first_letter)
+  	@submission.bdr = "Bill"
+  	notification_email = "bill@twilio.com"
+  else
+  	@submission.bdr = "error"
+  	notification_email = "casey@twilio.com"
+  end
+
+  email_text = 'You have a new FSR submission, please reach out within the next hour. Here is the info:
+  	Company: '+@submission.company+'
+  	Name: '+@submission.firstName+' '+@submission.lastName+'
+  	Email: '+@submission.emailAddress+'
+  	Description: '+@submission.description1+'
+  	All info: '+@submission.all_params
+  
+  begin 
+	  client = SendGrid::Client.new(api_user: 'caseyclegg', api_key: 'tigerdude48')
+	  client.send(SendGrid::Mail.new(to: notification_email, from: 'casey@twilio.com', subject: 'New FSR Submission', text: email_text))
+	  @submission.bdr += ', sent'
+  rescue
+  	@submission.bdr += ', error on sending'
+  end
+
+  @submission.save
+end
